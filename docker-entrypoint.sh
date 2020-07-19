0c3a9b35bfbbce892d476cf4094c602c3bff5344
sushant <sushant>
2020-07-19 19:58:27 +0530

    changed docker-compose.yaml

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 sed -i 's#127.0.0.1#my-cassandra#g' /app/config/cequel.yml
+until nc -vz my-cassandra 9042 2>/dev/null; do
+	  echo "Cassandra is not ready, sleeping."
+	    sleep 1
+    done
 rails cequel:keyspace:create
 rails cequel:migrate
 rails s -b 0.0.0.0
